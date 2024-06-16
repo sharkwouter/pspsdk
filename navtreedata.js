d97dbf9b87966dc2f5f4d38cee29a2399a140d97
sharkwouter <sharkwouter@users.noreply.github.com>
2024-06-16 10:59:09 +0000

    deploy: 227a0f4d8450780901fa28060b9129d663d2aed4

---
+++ navtreedata.js
@@ -0,0 +1,81 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "PSPSDK", "index.html", [
+    [ "PSP Software Development Kit", "index.html", "index" ],
+    [ "Topics", "topics.html", "topics" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", "globals_vars" ],
+        [ "Typedefs", "globals_type.html", "globals_type" ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", "globals_eval" ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"gdb-kernellib_8c.html#a25e2eacbf67f0a0132bf6550b5e31f2f",
+"group__Debug.html#ga0069d1f2833d025d9447202bb9a1db90",
+"group__IntrMan.html#ggae7e3c131fa486f14a43fb227f917235ea1db41e42b9a89fe7b2af6f2c091f925c",
+"group__ThreadMan.html#ga6eb030ea3ffd2dfb0be56e6a5b48900c",
+"guInternal_8h.html#acf32301ca9b2f3977bf01ea7ed9ae437",
+"pspatrac3_8h.html#a1177ae874062b93726113cf561c48600",
+"pspfpu_8c.html#a57d34d34fb1d922b723c97cf66c04d8b",
+"pspgu_8h.html#a5a795ed95e44b4760f2a3ad1cb3b5b72",
+"psphttp_8h.html#afa987bf7ecc1b4584fc06b5434c590f4",
+"pspkerror_8h.html#ada38ce7dd0ddff755524e736afd6fcaca841b8e30fc3ebaf78edbcb8d1fc7b8cb",
+"pspnand__driver_8h.html#a520e37eba201d2f3ec0e4e9d22abb4da",
+"psprtc_8h.html#a0d927b47867a3d7cdc599fe58a9f4d73",
+"pspusbbus_8h.html#a56ff382e0f27f09c99b938c096571713",
+"psputility__modules_8h.html#abf4216148b7b9c2b3c5c2fd758a059f9",
+"pspvaudio_8h.html#a15ffff1382485fb0a7cb1d087a044d6d",
+"socket_8h.html#a8f632f2857bd93af1f73295e405300ae",
+"structPspSysEventHandler.html",
+"structSceKernelSystemStatus.html#a8ffcd1a3ea9d3b1c4556df684c7bc5e5",
+"structScePspFMatrix3.html#abb9a6e35fabec80002f7d37d8f8d9001",
+"structUsbDriver.html#ae16871cccecc440a5c97bdcda3277934",
+"structpdpStatStruct.html#ab80ab6d245b63541024cfca712eb3065",
+"unionScePspVector3.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ navtreeindex22.js
@@ -0,0 +1,16 @@
+var NAVTREEINDEX22 =
+{
+"unionScePspVector3.html":[2,0,174],
+"unionScePspVector3.html#a09170fbb145054f7aa43df95cf1ec4e9":[2,0,174,2],
+"unionScePspVector3.html#a133bc30bfed575c37f3d7e2e51369f95":[2,0,174,0],
+"unionScePspVector3.html#aa565cde3f7776cdb108d103b6777fdbc":[2,0,174,3],
+"unionScePspVector3.html#aac213c90570d7b1568325e37d1806a2d":[2,0,174,1],
+"unionScePspVector4.html":[2,0,175],
+"unionScePspVector4.html#a1451ef9663ea5678527d026cdcfd51dc":[2,0,175,2],
+"unionScePspVector4.html#a2bd9286cec82c5db7a2d338ca017b374":[2,0,175,1],
+"unionScePspVector4.html#a368a3d789a9a6b1189fcb262b24c1c36":[2,0,175,0],
+"unionScePspVector4.html#ac7141b486cf0d6a5a8cc6a85a02120a8":[2,0,175,3],
+"unionnetData.html":[2,0,45],
+"unionnetData.html#a158aacc07726a07d88f1c88972a9ece8":[2,0,45,1],
+"unionnetData.html#a79e4035f31129bbbc4f075429de8b318":[2,0,45,0]
+};
